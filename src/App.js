0d68c770ebcd1da18b8471ed74bf3ffcc64beab8
Viktor Hetman <vitya.getman.79@gmail.com>
2023-02-19 21:40:27 +0200

    card directory w component and scss module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import Card from "./components/Card"
+import Card from "./components/Card/Card"
 import Cart from "./components/Cart"
 import Header from "./components/Header"
 
-function App() {
+
+const arr = [
+  {title: 'Nike Blazer Mid Suede', price: 300, imageUrl: '/img/sneakers/1.jpg'},
+  {title: 'Nike Air Max 270', price: 200, imageUrl: '/img/sneakers/2.jpg'},
+  {title: 'Nike Blazer Mid Suede', price: 150, imageUrl: '/img/sneakers/3.jpg'},
+  {title: 'Puma X Aka Boku Future Rider', price: 180, imageUrl: '/img/sneakers/4.jpg'}
+]
+
+function App() {  
   return (
     <div className="wrapper clear">
       <Cart />
@@ -15,8 +23,10 @@ function App() {
              <input placeholder="Search..."/>
           </div>
         </div>
-        <div className="d-flex">
-        <Card />
+        <div className="d-flex flex-wrap">
+        {arr.map((arr) => {
+          return <Card title={arr.title} price={arr.price} imageUrl={arr.imageUrl}/>
+        })}
         </div>
       </div>
     </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,29 @@
 import React from 'react'
+import styles from './Card.module.scss'
 
-function Card() {
+function Card({title, price, imageUrl}) {
+  const alertHandler = () => {
+    alert(title)
+  }
+
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="img/unliked.svg" alt="unliked-icon" />
       </div>
       <img
         width={133}
         height={122}
-        src="/img/sneakers/1.jpg"
+        src={imageUrl}
         alt="sneakers-1"
       />
-      <h5>Nike Blazer Mid Suede</h5>
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Price:</span>
-          <b>200$</b>
+          <b>{price} $</b>
         </div>
-        <button className="button">
+        <button className={styles.button} onClick={alertHandler}>
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
