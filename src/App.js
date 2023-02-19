b11b55e3235be8eb228644bf1687f38ea8664322
Viktor Hetman <vitya.getman.79@gmail.com>
2023-02-19 12:35:50 +0200

    some static and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
-
-
 function App() {
   return (
-    <div className="App">
-       
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={100} height={100} src="/img/logo.png" alt="logo"/>
+          <div>
+            <h3 className="text-uppercase">Sneakers Store</h3>
+            <p className="opacity-5">The best sneakers</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" alt="cart"/>
+            <span>100$</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.png" alt="user"/>
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1 className="mb-40">All sneakers</h1>
+        
+        <div className="d-flex">
+        <div className="card">
+          <img width={133} height={122} src="/img/sneakers/1.jpg" alt="sneakers-1"/>
+          <h5>Nike Blazer Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Price:</span>
+              <b>200$</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={122} src="/img/sneakers/2.jpg" alt="sneakers-1"/>
+          <h5>Nike Blazer Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Price:</span>
+              <b>200$</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={122} src="/img/sneakers/3.jpg" alt="sneakers-1"/>
+          <h5>Nike Blazer Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Price:</span>
+              <b>200$</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img width={133} height={122} src="/img/sneakers/4.jpg" alt="sneakers-1"/>
+          <h5>Nike Blazer Mid Suede</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Price:</span>
+              <b>200$</b>
+            </div>
+            <button className="button">
+              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+            </button>
+          </div>
+        </div>
+        </div>
+        
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
