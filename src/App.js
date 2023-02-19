a4b5ab9563c499898f0f7b3db9b4420f5af5d572
Viktor Hetman <vitya.getman.79@gmail.com>
2023-02-19 16:47:50 +0200

    the last static before refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="cartBar">
+          <h2 className="d-flex justify-between mb-30">Cart <img className="removeBtn cu-p" src="/img/remove.svg" alt="remove-button"/></h2>
+
+          <div className="items">
+          <div className="cartItem d-flex align-center mb-20">
+              <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Nike Blazer Mid Suede</p>
+                <b>200$</b>
+              </div>
+              <img className="removeBtn" src="/img/remove.svg" alt="remove-button"/>
+          </div>
+
+          <div className="cartItem d-flex align-center">
+              <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Nike Blazer Mid Suede</p>
+                <b>200$</b>
+              </div>
+              <img className="removeBtn" src="/img/remove.svg" alt="remove-button"/>
+          </div>
+          </div>
+        
+
+        <div className="cartTotal">
+          <ul>
+            <li>
+              <span>Total price:</span>
+              <div></div>
+              <b>400$</b>
+            </li>
+            <li>
+              <span>Discount 5%:</span>
+              <div></div>
+              <b>20$</b>
+            </li>
+          </ul>
+          <button className="greenButton">Checkout!<img src="/img/arrow.svg" alt="arrow"/></button>
+        </div>
+        </div>
+      </div>
+
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={100} height={100} src="/img/logo.png" alt="logo"/>
